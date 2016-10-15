c09bf2e9592c234c2f17b82ace18c0a9730542a7
gorillagunz <jovaunrobinson@Yahoo.com>
2016-10-14 20:59:24 -0500

    Delete maze.js

--- maze.js
+++
@@ -1,7 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", function()
-{
-
-	document.getElementById("boundary1").addEventListener("mouseover", function()
-		{ this.className += " youlose";});
-});
